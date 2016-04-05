51c12b93229b227703571bcdc04130257b5720ad
Nick Manton <nick@mantonbradbury.com>
2016-04-05 21:33:36 +0100

    change field from confirmed to pending
    this model used confirmed for the temporary flag, whereas the
    league model uses pending (with opposite values).

--- models/league_user.js
+++ models/league_user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.INTEGER(11),
       allowNull: false
     },
-    confirmed: {
+    pending: {
       type: DataTypes.INTEGER(11),
       allowNull: true
     }
