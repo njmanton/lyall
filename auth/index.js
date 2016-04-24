1bc0595de489340624797bc7791b9108b58c7548
Nick Manton <nick@mantonbradbury.com>
2016-04-24 12:42:01 +0100

    add strategies for Facebook and Google SSO

--- auth/index.js
+++ auth/index.js
@@ -1,19 +1,26 @@
-var models    = require('../models'),
-    bCrypt = require('bcrypt-nodejs'),
-    passport  = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+// jshint node: true, esversion: 6
+'use strict';
+
+var models              = require('../models'),
+    bCrypt              = require('bcrypt-nodejs'),
+    chalk               = require('chalk'),
+    passport            = require('passport'),
+    configAuth          = require('../config/auth_config'),
+    LocalStrategy       = require('passport-local').Strategy,
+    FacebookStrategy    = require('passport-facebook').Strategy,
+    GoogleStrategy      = require('passport-google-oauth').OAuth2Strategy;
 
 module.exports.createHash = function(password){
   return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-}
+};
 
 module.exports = function(app) {
 
   app.use(passport.initialize());
   app.use(passport.session());
 
-  passport.use('local', new LocalStrategy(
-    function(username, password, done) {
+  passport.use('local', new LocalStrategy({ passReqToCallback: true },
+    function(req, username, password, done) {
       models.User.findOne({
         where: { username: username }
       }).then(function(user) {
@@ -25,16 +32,106 @@ module.exports = function(app) {
           console.log('wrong password');
           return done(null, false, { message: 'incorrect password' });
         }
-        // nullify reset code, if present
-        user.update({ resetpwd: null });
+        user.update({ resetpwd: null }); // nullify reset code, if present
+        req.flash('success', 'logged in');
         return done(null, user);
 
       }).error(function(err) {
         return done(err);
-      })
+      });
     }
   ));
 
+  passport.use(new FacebookStrategy({
+    clientID          : configAuth.facebookAuth.clientID,
+    clientSecret      : configAuth.facebookAuth.clientSecret,
+    callbackURL       : configAuth.facebookAuth.callbackURL,
+    passReqToCallback : true
+  },
+    function(req, token, refreshToken, profile, done) {
+      process.nextTick(function() {
+        if (req.user) {
+          models.User.update({
+            facebook_id: profile.id
+          }, {
+            where: { id: req.user.id }
+          }).then(function(u) {
+            if (u) {
+              req.flash('info', 'Account now linked to your Facebook profile');
+              return done(null, req.user);
+            } else {
+              req.flash('error', 'Couldn\'t link your profile');
+              return done(null, req.user);
+            }
+          }).catch(function(e) {
+            return done(e);
+          });
+        } else {
+          // find the user in the database based on their facebook id
+          models.User.findOne({
+            where: { 'facebook_id': profile.id }
+          }).then(function(user) {
+            if (user) {
+              req.flash('success', 'Logged in via Facebook');
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Can\'t find matching FB user' });
+            }
+          }).catch(function(e) {
+            return done(e);
+          });          
+        }
+
+      });
+    }
+  ));
+
+  passport.use(new GoogleStrategy({
+    clientID          : configAuth.googleAuth.clientID,
+    clientSecret      : configAuth.googleAuth.clientSecret,
+    callbackURL       : configAuth.googleAuth.callbackURL,
+    passReqToCallback : true
+  },
+    function(req, token, refreshToken, profile, done) {
+      process.nextTick(function() {
+        if (req.user) {
+          models.User.update({
+            google_id: profile.id
+          }, {
+            where: { id: req.user.id }
+          }).then(function(u) {
+            if (u) {
+              req.flash('info', 'Account now linked to your Google+ profile');
+              return done(null, req.user);
+            } else {
+              req.flash('error', 'Couldn\'t link your profile');
+              return done(null, req.user);
+            }
+          }).catch(function(e) {
+            return done(e);
+          });
+        } else {
+          // find the user in the database based on their google id
+          models.User.findOne({
+            where: { 'google_id': profile.id }
+          }).then(function(user) {
+            if (user) {
+              req.flash('success', 'Logged in via Google');
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Can\'t find matching Google+ user' });
+            }
+          }).catch(function(e) {
+            return done(e);
+          });          
+        }
+
+      });
+    }
+  ));
+
+
+  // make user object available in handlebars views
   app.use(function (req, res, next) {
     if (!res.locals.user && req.user) {
       res.locals.user = req.user;
@@ -52,5 +149,5 @@ module.exports = function(app) {
     });
   });
 
-}
+};
 
