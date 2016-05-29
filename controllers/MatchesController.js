2a6936464629ebb66410a6c11cc2704ad6ab2cde
Nick Manton <nick@mantonbradbury.com>
2016-05-29 09:33:47 +0100

    add ajax route for heatmaps
    add a route to send JSON data to populate the highcharts heatmap

--- controllers/MatchesController.js
+++ controllers/MatchesController.js
@@ -137,27 +137,65 @@ module.exports = {
 
   }],
 
-  post_result: [utils.isAdmin, utils.isAjax, function(req, res) {
+  get_id_heatmap: [utils.isAjax, function(req, res, id) {
+    if (id < 1 || id > 51) { res.sendStatus(404); }
+
+    let qry = 'SELECT prediction, COUNT(id) AS cnt FROM predictions WHERE match_id=' + id + ' GROUP BY prediction';
+    var preds = models.sequelize.query(qry, { type: models.sequelize.QueryTypes.SELECT });
+    var result = models.Match.findById(id, {
+      attributes: ['result']
+    });
+    models.sequelize.Promise.join(
+      preds,
+      result,
+      function(preds, result) {
+        let hm_data = [],
+            sumprod = [0, 0],
+            total_preds = 0,
+            rs = [];
+        if (result.result) {
+          rs = result.result.split('-').map(x => x * 1);
+        }            
+        for (var i = preds.length - 1; i >= 0; i--) {
+          var goals = preds[i].prediction.split('-');
+          sumprod[0] += (preds[i].cnt * (goals[0] * 1));
+          sumprod[1] += (preds[i].cnt * (goals[1] * 1));
+          total_preds += preds[i].cnt;
+          hm_data.push([goals[0] * 1, goals[1] * 1, preds[i].cnt]);
+        };
+        res.send({
+          counts: hm_data,
+          mean: sumprod.map(x => parseFloat((x / total_preds).toFixed(2))),
+          result: rs
+        });
+      })
+  }],
+
+  post_id_result: [utils.isAdmin, function(req, res, id) {
     // ajax post to update score
-    // post format { mid: <match id>, result: <match result> }
+    // post format { id: <match id>, result: <match result> }
     var result = req.body.result;
     models.Match.update({
       result: result
     }, {
-      where: { id: req.body.id }
+      where: { id: id }
     }).then(function(rows) {
       // loop through each promise to update prediction scores
       var preds = models.Pred.findAll({
-        where: { match_id: req.body.mid }
+        where: { match_id: id }
       });
+      var p = 0;
       models.sequelize.Promise.each(preds, function(pred) {
         let pts = utils.calc(pred.prediction, result, pred.joker);
         pred.update({
           points: pts
-        });
-      });
-      res.send(rows > 0);
-    });
+        })
+        p++;
+      }).then(e => {
+        req.flash('info', 'Result set to ' + result + '. ' + e.length + ' predictions updated');
+        res.redirect('/matches/' + id);        
+      })
+    })
   }]
 
 };
