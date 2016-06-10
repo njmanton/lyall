1a6fbeabb222ad3e3a11049ffc21e44069b17a96
Nick Manton <nick@mantonbradbury.com>
2016-06-10 08:01:47 +0100

    FEAT: add entering/editing of scorers and goals
    add controller and view to allow goals/scorers to be added

---
+++ controllers/GoalsController.js
@@ -0,0 +1,44 @@
+// jshint node: true, esversion: 6
+'use strict';
+
+var models  = require('../models'),
+    utils   = require('../utils');
+
+module.exports = {
+
+  delete_id: [utils.isAdmin, function(req, res, id) {
+    models.Goal.destroy({
+      where: { id: id }
+    }).then(r => {
+      console.log('deleted a row', r);
+      res.send(r > 0);
+    })
+  }],
+
+  post_add: [utils.isAdmin, function(req, res) {
+    if (!req.body.scorer || !req.body.team || req.body.time < 1 || req.body.time > 90) {
+      req.flash('error', 'something wrong with that data');
+      res.redirect(req.headers.referer);
+    } else {
+      models.Goal.create({
+        match_id: req.body.match_id,
+        team_id: req.body.team,
+        scorer: req.body.scorer,
+        time: req.body.time,
+        tao: req.body.tao,
+        type: req.body.type
+      }).then(goal => {
+        if (goal) {
+          req.flash('success', 'Goal added');
+        } else {
+          req.flash('error', 'Couldn\'t save data');
+          console.log('err');
+        }
+        res.redirect(req.headers.referer);
+      })      
+    }
+
+    
+  }]
+
+}
