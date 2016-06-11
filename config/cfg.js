deab664a45518d5724f6011a430a676ed40b46ee
Nick Manton <nick@mantonbradbury.com>
2016-06-11 16:13:11 +0100

    allow curl debugging for ajax routes

--- config/cfg.js
+++ config/cfg.js
@@ -3,7 +3,7 @@
 var cfg = {
 
   ignoreExpiry:   0, // ignores deadlines
-  allowCurlAjax:  0, // allow a curl request sent to an ajax-only route
+  allowCurlAjax:  1, // allow a curl request sent to an ajax-only route
   allowCurlAuth:  0, // allow a curl request sent to an authorised route
   allowCurlAdmin: 0, // allow a curl request sent to an admin-only route
   allowCurlAnon:  0  // allow a curl request send to an anon-only route
